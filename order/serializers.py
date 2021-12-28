8acd80527fae4dc6bd31dfd08ece15a8851a1ad5
Hernandes <lertanisslobr@gmail.com>
2021-12-28 11:38:56 -0300

    ajeitando os serializers

--- order/serializers.py
+++ order/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Order
 class OrderSerializer(serializers.ModelSerializer):
-    model = Order
-    fields= '__all__'
+    class Meta:
+        model = Order
+        fields= '__all__'
