d05756e490fdc61cbbcd69bd5a333516e3e8cbc3
Hernandes <lertanisslobr@gmail.com>
2021-12-28 11:43:49 -0300

    colocando os str

--- products/models.py
+++ products/models.py
@@ -3,9 +3,15 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=250)
+
+    def __str__(self) -> str:
+        return self.name
 class Product(models.Model):
     name = models.CharField(max_length=250)
     description = models.TextField()
     price = models.DecimalField(max_digits=2,decimal_places=2)
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
+    
+    def __str__(self) -> str:
+        return self.name
