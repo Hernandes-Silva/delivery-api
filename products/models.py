47d9493b2ae676290873d33696dc87ee2ad5a088
Hernandes <lertanisslobr@gmail.com>
2021-12-28 12:11:39 -0300

    Modificando o field price

--- products/models.py
+++ products/models.py
@@ -9,9 +9,9 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=250)
     description = models.TextField()
-    price = models.DecimalField(max_digits=2,decimal_places=2)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    
+
     def __str__(self) -> str:
         return self.name
