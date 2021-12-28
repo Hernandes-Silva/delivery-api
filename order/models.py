fc813a37f1c009288ba09d09f13030fb1532e26e
Hernandes <lertanisslobr@gmail.com>
2021-12-28 11:03:08 -0300

    criando os models e fazendo os makemigrations

--- order/models.py
+++ order/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+from products.models import Product
+
 # Create your models here.
+class Order(models.Model):
+    product = models.ForeignKey(Product, related_name='orders', on_delete=models.CASCADE)
+    paid = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=2,decimal_places=2)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
